7fd6b0b514f5de649b21ee5b7e92c9f1a2f94748
Joshua Lansford <Joshua@Lansfords.com>
2023-06-22 17:52:37 -0400

    I got the scrolling of the list area to work correctly now.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,10 +6,11 @@ import List from './List';
 import Toolbar from './Toolbar';
 
 const App: React.FC = () => {
-  const listItems = ['Item 1', 'Item 2', 'Item 3'];
+  //const listItems = ['Item 1', 'Item 2', 'Item 3', 'a', 'b', 'c', 'e', 'j','k', 'Item 3', 'a', 'b', 'c', 'e', 'f', 'g','h','i','j','k', 'a', 'b', 'c', 'e', 'f', 'g','h','i','j','k'];
+  const listItems = ['Item 1', 'Item 2', 'Item 3', 'a', 'b', 'c', 'e', 'j','k', 'Item 3', 'a', 'b', 'c', 'e', 'f', 'g','h','i','j','k', 'Item 3', 'a', 'b', 'c', 'e', 'f', 'g','h','i','j','k', 'a', 'b', 'c', 'e', 'f', 'g','h','i','j','k'];
 
   return (
-    <div>
+    <div className="h-screen flex flex-col py-4">
       <header className="py-4 bg-gray-200">
         <nav className="container mx-auto">
           <ul className="flex space-x-4">
@@ -19,7 +20,7 @@ const App: React.FC = () => {
         </nav>
       </header>
 
-      <main className="container mx-auto mt-8">
+      <main className="overflow-y-scroll container flex-grow mx-auto mt-8 py-4 bg-white">
         <List items={listItems} />
       </main>
 
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,7 +7,7 @@ interface ListProps {
 
 const List: React.FC<ListProps> = ({ items }) => {
   return (
-    <div className="h-48 overflow-y-scroll bg-white border border-gray-300 p-4">
+    <div>
       <ul className="list-disc list-inside">
         {items.map((item, index) => (
           <li key={index}>{item}</li>
