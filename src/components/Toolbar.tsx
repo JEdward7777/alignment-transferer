551e282ab005b5005af5b898602de2548c318382
Joshua Lansford <Joshua@Lansfords.com>
2023-06-23 11:28:02 -0400

    extracted code to utility

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,26 +1,18 @@
 // Toolbar.tsx
 import React from 'react';
+import { handleFileChange } from '../utils/load_file';
 
 const Toolbar: React.FC = () => {
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log( "in handleFileChange" )
-        const file = event.target.files?.[0]; // Get the first selected file
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = handleFileRead;
-            reader.readAsText(file); // Read the file as text
-        }
-    };
 
-    const handleFileRead = (event: ProgressEvent<FileReader>) => {
-        console.log( "In handleFileRead" )
-        const fileContent = event.target?.result as string; // Retrieve the text content of the file
-        console.log(fileContent); // Perform any further processing or store the content as needed
+
+    const contents_callback = (fileContent: String) => {
+        console.log( "in callbacki" );
+        //console.log( fileContent );
     };
 
     return (
         <div className="text-center">
-            <input type="file" onChange={handleFileChange} accept=".usfm" className="hidden" id="file-input" />
+            <input type="file" onChange={handleFileChange(contents_callback)} accept=".usfm" className="hidden" id="file-input" />
             <label htmlFor="file-input" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded cursor-pointer">
                 Add Resource
             </label>
---
+++ src/utils/load_file.ts
@@ -0,0 +1,17 @@
+export function handleFileChange( callback: (arg: string) => void ): (event: React.ChangeEvent<HTMLInputElement>) => void {
+
+    const _handleFileRead = (event: ProgressEvent<FileReader>) => {
+        const fileContent = event.target?.result as string; // Retrieve the text content of the file
+        callback(fileContent); // Perform any further processing or store the content as needed
+    };
+    const _handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0]; // Get the first selected file
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = _handleFileRead;
+            reader.readAsText(file); // Read the file as text
+        }
+    };
+
+    return _handleFileChange;
+}
