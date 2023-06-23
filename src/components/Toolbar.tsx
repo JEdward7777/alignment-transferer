aeb73b56c1d3b74a38cbaecf52fd4f38a8397b83
Joshua Lansford <Joshua@Lansfords.com>
2023-06-23 11:59:41 -0400

    The loading system now supports multiple files to be selected at once.

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -5,14 +5,14 @@ import { handleFileChange } from '../utils/load_file';
 const Toolbar: React.FC = () => {
 
 
-    const contents_callback = (fileContent: String) => {
-        console.log( "in callbacki" );
-        //console.log( fileContent );
+    const contents_callback = (filename: String, fileContent: String) => {
+        console.log( `in callback filename is ${filename}` );
+        console.log( fileContent );
     };
 
     return (
         <div className="text-center">
-            <input type="file" onChange={handleFileChange(contents_callback)} accept=".usfm" className="hidden" id="file-input" />
+            <input type="file" onChange={handleFileChange(contents_callback)} accept=".usfm" className="hidden" id="file-input" multiple />
             <label htmlFor="file-input" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded cursor-pointer">
                 Add Resource
             </label>
--- src/utils/load_file.ts
+++ src/utils/load_file.ts
@@ -1,15 +1,21 @@
-export function handleFileChange( callback: (arg: string) => void ): (event: React.ChangeEvent<HTMLInputElement>) => void {
+export function handleFileChange( callback: (filename: String, contents: string) => void ): ((event: React.ChangeEvent<HTMLInputElement>) => void) {
 
-    const _handleFileRead = (event: ProgressEvent<FileReader>) => {
-        const fileContent = event.target?.result as string; // Retrieve the text content of the file
-        callback(fileContent); // Perform any further processing or store the content as needed
-    };
+    const _handleFileReadFor = (filename: String ): ((event: ProgressEvent<FileReader>) => void) => {
+        const _handleFileRead = (event: ProgressEvent<FileReader>) => {
+            const fileContent = event.target?.result as string; // Retrieve the text content of the file
+            callback(filename,fileContent); // Perform any further processing or store the content as needed
+        };
+        return _handleFileRead;
+    }
     const _handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0]; // Get the first selected file
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = _handleFileRead;
-            reader.readAsText(file); // Read the file as text
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            for( let i = 0; i < files.length; ++i ){
+                const file = files[i];
+                const reader = new FileReader();
+                reader.onload = _handleFileReadFor(file.name);
+                reader.readAsText(file); // Read the file as text
+            }
         }
         // Clear the input field
         event.target.value = '';
