42dbf69281f21b4dbb5c3a277aaf8d387ecdd82b
Joshua Lansford <Joshua@Lansfords.com>
2023-06-30 13:41:11 -0400

    Switched to react-data-grid

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { FaSortUp, FaSortDown } from 'react-icons/fa';
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  SortingState,
-  useReactTable,
-} from '@tanstack/react-table'
+import 'react-data-grid/lib/styles.css';
+import DataGrid from 'react-data-grid';
+
 
 interface DataObject {
   group: string;
@@ -18,81 +12,12 @@ interface TableProps {
   data: DataObject[];
 }
 
+
 export default function List({ data }: TableProps) {
-  const rerender = React.useReducer(() => ({}), {})[1]
+  const columns = [
+    { key: 'group', name: 'Group' },
+    { key: 'toc3', name: 'Name' }
+  ];
 
-  const [sorting, setSorting] = React.useState<SortingState>([])
-
-  const columns = React.useMemo<ColumnDef<DataObject>[]>(
-    () => [
-      {
-        header: 'Group',
-        accessorKey: 'group',
-      },{
-        header: 'toc3',
-        accessorKey: 'toc3',
-      }
-    ],[]
-  )
-
-  const table = useReactTable({
-    data,
-    columns,
-    state: {
-      sorting,
-    },
-    onSortingChange: setSorting,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-  });
-
-  
-  return (
-    <div className="p-2">
-      <table className="table-fixed">
-      <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                return (
-                  <th key={header.id} colSpan={header.colSpan} className="w-1/4">
-                    {header.isPlaceholder ? null : (
-                      <div
-                        {...{
-                          className: header.column.getCanSort()
-                            ? 'cursor-pointer select-none'
-                            : '',
-                          onClick: header.column.getToggleSortingHandler(),
-                        }}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                        {{
-                          asc: <FaSortUp className="inline-block ml-1" />,
-                          desc: <FaSortDown className="inline-block ml-1" />,
-                        }[header.column.getIsSorted() as string] ?? null}
-                      </div>
-                    )}
-                  </th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map(row => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id} className="border px-4 py-2">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+  return <DataGrid columns={columns} rows={data} />;
 }
